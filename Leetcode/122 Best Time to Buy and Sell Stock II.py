4f1af29d9c04bb7b538bf8a7a348acf72ac47ac1
sy <shengy0726@163.com>
2016-07-14 17:30:21 +0800

    array

--- Leetcode/122 Best Time to Buy and Sell Stock II.py
+++ Leetcode/122 Best Time to Buy and Sell Stock II.py
@@ -10,3 +10,6 @@ class Solution(object):
             if diff > 0:
                 max_sum += diff
         return max_sum
+
+    def maxProfit(self, prices):
+        return sum(max(prices[i + 1] - prices[i], 0) for i in range(len(prices) - 1))
