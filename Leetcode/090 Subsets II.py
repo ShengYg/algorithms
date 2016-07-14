88c3cb7778a87dfdc20b4fa61c9b610b6fc1bd52
sy <shengy0726@163.com>
2016-07-14 16:07:52 +0800

    array

--- Leetcode/090 Subsets II.py
+++ Leetcode/090 Subsets II.py
@@ -1,4 +1,5 @@
 class Solution(object):
+#method 1
     def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
@@ -8,4 +9,41 @@ class Solution(object):
         totalset = [[]]
         for num in nums:
             totalset += [i+[num] for i in totalset if i+[num] not in totalset]
+        return totalset
+
+#method 2
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        totalset = [[]]
+        i = 0
+        while i < len(nums):
+            count = 0
+            while i+count<len(nums) and nums[i+count]==nums[i]:
+                count += 1          
+            for j in range(count):
+                totalset += [instance + [nums[i]] for instance in totalset if instance + [nums[i]] not in totalset]
+            i += count
+        return totalset
+
+#method 3
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        totalset = [[]]
+        i = 0
+        while i < len(nums):
+            count = 0
+            while i+count<len(nums) and nums[i+count]==nums[i]:
+                count += 1
+            for _, instance in enumerate(totalset[:]):
+                for j in range(count):
+                    totalset += [instance + [nums[i]]*(j+1) ]
+            i += count
         return totalset
