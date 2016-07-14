b90926e3f909b4e02ebabc0b3d2d4e09f4f55dba
sy <shengy0726@163.com>
2016-07-14 14:40:09 +0800

    array

--- Leetcode/079 Word Search.py
+++ Leetcode/079 Word Search.py
@@ -27,3 +27,22 @@ class Solution(object):
         or self.exist_word(board, word, i - 1, j, idx + 1) or self.exist_word(board, word, i + 1, j, idx + 1)
         board[i][j] = word[idx]
         return exist
+
+	#method 2
+    def exist_word(self, board, word, i, j):
+        if board[i][j] == word[0]:
+            if not word[1:]:
+                return True
+            board[i][j] = "#"
+            if i > 0 and self.exist_word(board, word[1:], i-1, j):
+                return True
+            if i < len(board)-1 and self.exist_word(board, word[1:], i+1, j):
+                return True
+            if j > 0 and self.exist_word(board, word[1:], i, j-1):
+                return True
+            if j < len(board[0])-1 and self.exist_word(board, word[1:], i, j+1):
+                return True
+            board[i][j] = word[0]
+            return False
+        else:
+            return False
