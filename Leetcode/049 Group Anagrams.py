bb555f76f505851106de34cb217d3f320c25ba2f
sy <shengy0726@163.com>
2016-07-18 14:58:20 +0800

    Hash Table

---
+++ Leetcode/049 Group Anagrams.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        prime = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
+        result = {}
+        for s in strs:
+            if s == "":
+                key = 0
+            else:
+                key = reduce((lambda x,y:x*y), [prime[ord(i)-97] for i in s])
+            result[key] = result.get(key, []) + [s]
+        return [i for i in result.values()]
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hashmap={}
+        for i in strs:
+            key = ''.join(sorted(i))
+            hashmap[key] = hashmap.get(key, []) + [i]
+        return [v for v in hashmap.values()]
