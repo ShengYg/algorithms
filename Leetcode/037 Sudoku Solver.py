a7e02fbbb2df31c26bab094cebe742597eea6785
sy <shengy0726@163.com>
2016-07-18 13:42:55 +0800

    Hash Table

--- Leetcode/037 Sudoku Solver.py
+++ Leetcode/037 Sudoku Solver.py
@@ -4,8 +4,6 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
-        if len(board) == 0:
-            return
         for row in xrange(len(board)):
             board[row] = list(board[row])
         self.solve(board)
@@ -41,3 +39,32 @@ class Solution(object):
                 if board[row][col] == c:
                     return False
         return True
+
+#method 2 232ms
+class Solution(object):
+    def solveSudoku(self, board):
+        self.board = board
+        self.row = [[1] * 10 for i in range(9)]
+        self.col = [[1] * 10 for j in range(9)]
+        self.box = [[[1] * 10 for j in range(3)] for i in range(3)]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.': continue
+                v = int(board[i][j])
+                self.row[i][v] = self.col[j][v] = self.box[i/3][j/3][v] = 0
+        self.BT(0, 0)
+
+    def BT(self, i, j):
+        if i == 9:
+            return True
+        if self.board[i][j] != '.':
+            return self.BT(*((i, j+1) if j < 8 else (i+1, 0)))
+
+        for v in range(1, 10):
+            if self.row[i][v] and self.col[j][v] and self.box[i/3][j/3][v]:
+                self.board[i][j] = str(v)
+                self.row[i][v] = self.col[j][v] = self.box[i/3][j/3][v] = 0
+                if self.BT(*((i, j+1) if j < 8 else (i+1, 0))): return True
+                self.row[i][v] = self.col[j][v] = self.box[i/3][j/3][v] = 1
+        self.board[i][j] = '.'
+        return False
