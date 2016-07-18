19c3ee35d66ccd99a089ff18bdb9ca6e67bce35f
sy <shengy0726@163.com>
2016-07-18 17:28:40 +0800

    Hash Table

--- Leetcode/076 Minimum Window Substring.py
+++ Leetcode/076 Minimum Window Substring.py
@@ -18,3 +18,38 @@ class Solution(object):
                 if not J or j - i <= J - I:
                     I, J = i, j
         return s[I:J]
+
+
+from collections import defaultdict
+import sys
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        required, min_len = defaultdict(int), sys.maxint
+        
+        for ch in t:
+            required[ch] += 1
+        start = min_start = 0
+        min_end, count = -len(s) - 1, len(required)
+
+        for i in xrange(len(s)):
+            required[s[i]] -= 1
+            if required[s[i]] == 0:
+                if count == 1:
+                    for j in xrange(start, i+1):
+                        required[s[j]] += 1
+                        if required[s[j]] > 0:
+                            length = i - j
+                            if length < min_len:
+                                min_len = length
+                                min_start, min_end = j, i
+                            start = j + 1
+                            break
+                else:
+                    count -= 1
+
+        return s[min_start : min_end+1]
