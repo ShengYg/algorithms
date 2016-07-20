0a678eaa71a7e8aa4cc9fcda475f486b44156688
sy <shengy0726@163.com>
2016-07-20 17:18:38 +0800

    math

--- Leetcode/204 Count Primes.py
+++ Leetcode/204 Count Primes.py
@@ -36,3 +36,26 @@ class Solution(object):
                 isPrime[i**2: n: i] = [False] * len(isPrime[i * i: n: i])
             i += 1
         return sum(isPrime) - 2
+
+
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3:
+            return 0
+        if n < 4:
+            return 1
+        if n < 10:
+            return n/2
+        isPrime = [True] * n
+        i = 3
+        while i * i < n:
+            if isPrime[i]:
+                length = len(isPrime[i * i: n: 2*i])
+                isPrime[i**2: n: 2*i] = [False] * length
+            i += 2
+        return sum(isPrime) - int(round(float(n) / 2))
