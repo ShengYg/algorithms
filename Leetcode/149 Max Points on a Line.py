e3453f6038b62a5389ec4b07603a6a9b85b616d6
sy <shengy0726@163.com>
2016-07-20 10:30:09 +0800

    math

---
+++ Leetcode/149 Max Points on a Line.py
@@ -0,0 +1,41 @@
+# Definition for a point.
+# class Point(object):
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+
+class Solution(object):
+    def maxPoints(self, points):
+        """
+        :type points: List[Point]
+        :rtype: int
+        """
+        if not points:
+            return 0
+        L = len(points)
+        if L < 2:
+            return 1
+        result = 0
+        for i in range(L):
+            myMap, verticle, duplicate = {}, 0 ,0
+            for j in range(i+1,L):
+                a = points[i]
+                b = points[j]
+                if a.x == b.x:
+                    if a.y != b.y:
+                        verticle += 1
+                    else:
+                        duplicate += 1
+                else:
+                    slope = float(b.y - a.y) / float(b.x - a.x)
+                    myMap[slope] = myMap.setdefault(slope,0) + 1
+            
+            try:
+                slopeMax = max(myMap.values())
+            except:
+                slopeMax = 0
+            maxPointOnLine = max(slopeMax, verticle) + duplicate
+            if maxPointOnLine > result:
+                result = maxPointOnLine
+        
+        return result+1
