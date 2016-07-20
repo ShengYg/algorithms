afe0d7f94215e198638d88e1b8548c76e6adbbf7
sy <shengy0726@163.com>
2016-07-20 11:21:15 +0800

    math

--- Leetcode/166 Fraction to Recurring Decimal.py
+++ Leetcode/166 Fraction to Recurring Decimal.py
@@ -17,3 +17,28 @@ class Solution(object):
         result.insert(idx + 3, '(')
         result.append(')')
         return ''.join(result).replace('(0)', '').rstrip('.')
+
+
+class Solution(object):
+    def fractionToDecimal(self, numerator, denominator):
+        """
+        :type numerator: int
+        :type denominator: int
+        :rtype: str
+        """
+        if numerator == 0:
+            return '0'
+        n, remainder = divmod(abs(numerator), abs(denominator))
+        sign = '-' if (numerator < 0) ^ (denominator < 0) else ''
+        if remainder == 0:
+            return ''.join([sign, str(n)])
+        result = [sign, str(n), '.']
+        stack = []
+        while remainder not in stack:
+            stack.append(remainder)
+            n, remainder = divmod(remainder*10, abs(denominator))
+            result.append(str(n))
+        idx = stack.index(remainder)
+        result.insert(idx + 3, '(')
+        result.append(')')
+        return ''.join(result).replace('(0)', '')
