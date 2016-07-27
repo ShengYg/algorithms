4035de8249f71222129852bc60f855bb8a7c839b
sy <shengy0726@163.com>
2016-07-27 22:29:12 +0800

    math

--- Leetcode/263 Ugly Number.py
+++ Leetcode/263 Ugly Number.py
@@ -15,3 +15,23 @@ class Solution(object):
         while not num % 5:
             num /= 5
         return  num == 1
+
+
+
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        if num == 1:
+            return True
+        while not num % 2:
+            num = num >> 1		##faster
+        while not num % 3:
+            num /= 3
+        while not num % 5:
+            num /= 5
+        return  num == 1
