b881b051b5cd288e5b66d0dd3c38663d6a32182c
sy <shengy0726@163.com>
2016-07-29 15:36:44 +0800

    string

--- Leetcode/032 Longest Valid Parentheses.py
+++ Leetcode/032 Longest Valid Parentheses.py
@@ -9,18 +9,18 @@ class Solution(object):
             if s[i] == '(':
                 st.append(i)
             else:
-                if len(st):
+                if st:
                     if s[st[-1]] == '(':
                         st.pop()
                     else:
                         st.append(i)
                 else:
                     st.append(i)
-        if not len(st):
+        if not st:
             longest = n
         else:
             a, b = n, 0
-            while len(st):
+            while st:
                 b = st[-1]
                 st.pop()
                 longest = max(longest, a - b - 1)
@@ -50,4 +50,23 @@ class Solution(object):
                         longest[i] = longest[i-1] + 2 + (longest[i-longest[i-1]-2] \
                         if i-longest[i-1]-2 >= 0 else 0)
                         curMax = max(longest[i],curMax)
+        return curMax
+
+
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        if n <= 1:
+            return 0
+        curMax, longest = 0, [0]*n
+        for i in range(1, n):
+            if s[i] == ')' and i-longest[i-1]-1 >= 0 and s[i-longest[i-1]-1] == '(':
+                longest[i] = longest[i-1] + 2 + (longest[i-longest[i-1]-2] \
+                if i-longest[i-1]-2 >= 0 else 0)
+                curMax = max(longest[i],curMax)
         return curMax
