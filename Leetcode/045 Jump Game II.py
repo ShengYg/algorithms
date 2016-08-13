8cf92805feef789f347d0532610c53795cdaaaf6
Sheng Yang <shengy0726@gmail.com>
2016-08-13 10:50:36 +0800

    array

--- Leetcode/045 Jump Game II.py
+++ Leetcode/045 Jump Game II.py
@@ -16,4 +16,20 @@ class Solution(object):
                 return -1
         return minstep
 
-
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxreach, edge, minstep, n = nums[0], 0, 0, len(nums)
+        for i in range(1, n):
+            if i > edge:
+                minstep += 1
+                edge = maxreach
+                if edge >= n - 1:
+                    return minstep
+            maxreach = max(maxreach, nums[i] + i)
+            if maxreach >= n - 1:
+                return minstep + 1
+        return minstep
