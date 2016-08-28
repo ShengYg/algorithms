2ced136cb119033621d7e08f98c3bf7669bb7f32
ShengYg <shengy0726@gmail.com>
2016-08-28 16:06:14 +0800

    Backtracking

---
+++ Leetcode/046 Permutations.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.Recursive(nums, 0, result)
+        return result
+
+    def Recursive(self, nums, begin, result):
+        if begin >= len(nums) - 1:
+            result.append(nums)
+            return
+        for i in range(begin, len(nums)):
+            nums[begin], nums[i] = nums[i], nums[begin]
+            self.Recursive(nums[:], begin + 1, result)
+            nums[begin], nums[i] = nums[i], nums[begin]
+
