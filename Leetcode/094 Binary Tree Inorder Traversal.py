738d3e24e8657e6bc5959de8cafd6a7147ba68b9
ShengYg <shengy0726@gmail.com>
2016-11-19 16:32:02 +0800

    tree

--- Leetcode/094 Binary Tree Inorder Traversal.py
+++ Leetcode/094 Binary Tree Inorder Traversal.py
@@ -21,3 +21,22 @@ class Solution(object):
         self.inorder(root.left, nodes)
         nodes.append(root.val)
         self.inorder(root.right, nodes)
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        nodes, toVisit = [], []
+        curNode = root
+        while curNode or len(toVisit) != 0:
+            if curNode:
+                toVisit.append(curNode)
+                curNode = curNode.left
+            else:
+                curNode = toVisit[-1]
+                toVisit.pop()
+                nodes.append(curNode.val)
+                curNode = curNode.right
+        return nodes
