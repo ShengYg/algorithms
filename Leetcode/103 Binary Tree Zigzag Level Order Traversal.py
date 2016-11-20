8bb0105218a601a01bab75abb0aa6209aa705fa7
ShengYg <shengy0726@gmail.com>
2016-11-20 16:06:17 +0800

    bfs

--- Leetcode/103 Binary Tree Zigzag Level Order Traversal.py
+++ Leetcode/103 Binary Tree Zigzag Level Order Traversal.py
@@ -32,3 +32,39 @@ class Solution(object):
             res.append(curLevel)
             lefttoright = not lefttoright
         return res
+
+## deque
+from collections import deque
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue = deque()
+        queue.append(root)
+        res = []
+        lefttoright = True
+        while len(queue):
+            curLevel = []
+            for i in range(len(queue)):
+                if not lefttoright:
+                    curNode = queue.popleft()
+                    curLevel.append(curNode.val)
+                    if curNode.right:
+                        queue.append(curNode.right)
+                    if curNode.left:
+                        queue.append(curNode.left)
+                else:
+                    curNode = queue.pop()
+                    curLevel.append(curNode.val)
+                    if curNode.left:
+                        queue.appendleft(curNode.left)
+                    if curNode.right:
+                        queue.appendleft(curNode.right)
+                    
+            res.append(curLevel)
+            lefttoright = not lefttoright
+        return res
