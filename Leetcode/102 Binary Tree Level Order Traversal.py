3fe7f22381086c23a7e0a9b63916c912d21d5525
ShengYg <shengy0726@gmail.com>
2016-11-20 15:13:01 +0800

    bfs

--- Leetcode/102 Binary Tree Level Order Traversal.py
+++ Leetcode/102 Binary Tree Level Order Traversal.py
@@ -24,4 +24,26 @@ class Solution(object):
         self.recursive(res, root.left, level + 1)
         self.recursive(res, root.right, level + 1)
 
-
+## BFS
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue = []
+        queue.append(root)
+        res = []
+        while len(queue):
+            curLevel = []
+            for i in range(len(queue)):
+                curNode = queue.pop(0)
+                curLevel.append(curNode.val)
+                if curNode.left:
+                    queue.append(curNode.left)
+                if curNode.right:
+                    queue.append(curNode.right)
+            res.append(curLevel)
+        return res
