6331686ce5c8a5c89fba4b9b3d4bc4bd0167dbb4
ShengYg <shengy0726@gmail.com>
2016-11-20 16:21:53 +0800

    tree

--- Leetcode/104 Maximum Depth of Binary Tree.py
+++ Leetcode/104 Maximum Depth of Binary Tree.py
@@ -1,3 +1,4 @@
+## DFS space:O(lgn)
 class Solution(object):
     def maxDepth(self, root):
         """
@@ -5,3 +6,25 @@ class Solution(object):
         :rtype: int
         """
         return 0 if not root else max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+## BFS space:O(n)
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        queue = []
+        queue.append(root)
+        res = 0
+        while len(queue):
+            res += 1
+            for i in range(len(queue)):
+                curNode = queue.pop(0)
+                if curNode.left:
+                    queue.append(curNode.left)
+                if curNode.right:
+                    queue.append(curNode.right)
+        return res
