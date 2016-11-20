1568d8d08f3f3f21279d9aa8bc577de84297a8a0
ShengYg <shengy0726@gmail.com>
2016-11-20 14:40:20 +0800

    tree

--- Leetcode/101 Symmetric Tree.py
+++ Leetcode/101 Symmetric Tree.py
@@ -19,3 +19,43 @@ class Solution(object):
         if left.val != right.val:
             return False
         return self.branch(left.left, right.right) and self.branch(right.left, left.right)
+
+class Solution(object):
+    def isSymmetric(self, root):
+        if not root:
+            return True
+    
+        stack = []
+        if root.left:
+            if not root.right:
+                return False
+            stack.append(root.left)
+            stack.append(root.right)
+        elif root.right:
+            return False
+            
+        while len(stack):
+            if len(stack) % 2:
+                return False
+            right = stack.pop()
+            left = stack.pop()
+            if right.val != left.val:
+                return False
+            
+            if left.left:
+                if not right.right:
+                    return False
+                stack.append(left.left)
+                stack.append(right.right)
+            elif right.right:
+                return False
+            
+            if left.right:
+                if not right.left:
+                    return False
+                stack.append(left.right)
+                stack.append(right.left)
+            elif right.left:
+                return False
+    
+        return True
