f2f7325ad4c53a64a299afa02378b6b11157dec8
ShengYg <shengy0726@gmail.com>
2016-11-21 15:06:55 +0800

    BBT

--- Leetcode/110 Balanced Binary Tree.py
+++ Leetcode/110 Balanced Binary Tree.py
@@ -22,3 +22,27 @@ class Solution(object):
             if right != -1:
                 return 1 + max(left, right) if abs(left - right) <= 1 else -1
         return -1
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        try:
+            return self.getDepth(root) != -1
+        except:
+            return False
+    
+    def getDepth(self, root):
+        if not root:
+            return 0
+        left = self.getDepth(root.left)
+        if left == -1:
+            raise Exception
+        else:
+            right = self.getDepth(root.right)
+            if right == -1:
+                raise Exception
+            else:
+                return 1 + max(left, right) if abs(left - right) <= 1 else -1
