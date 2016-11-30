e5d9aa05807dbd76a30db3d52f364a032bb11ac6
ShengYg <shengy0726@gmail.com>
2016-11-30 12:22:53 +0800

    string

--- Leetcode/230 Kth Smallest Element in a BST.py
+++ Leetcode/230 Kth Smallest Element in a BST.py
@@ -23,3 +23,25 @@ class Solution(object):
         if not root:
             return 0
         return 1 + self.count(root.left) + self.count(root.right)
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        toVisit = []
+        curNode = root
+        while curNode or len(toVisit) != 0:
+            if curNode:
+                toVisit.append(curNode)
+                curNode = curNode.left
+            else:
+                curNode = toVisit[-1]
+                k -= 1
+                if not k:
+                    return curNode.val
+                toVisit.pop()
+                curNode = curNode.right
+        return -1
