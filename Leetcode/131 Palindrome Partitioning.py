4bbfbc30d03339ac9b844d5850db088ecac0027f
ShengYg <shengy0726@gmail.com>
2016-12-02 16:48:16 +0800

    backtracking

---
+++ Leetcode/131 Palindrome Partitioning.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        res = []
+        self.dfs(s, [], res)
+        return res
+
+    def dfs(self, s, path, res):
+        if not s:
+            res.append(path)
+            return
+        for i in range(1, len(s)+1):
+            a = s[:i]
+            if a == a[::-1]:
+                self.dfs(s[i:], path + [s[:i]], res)
