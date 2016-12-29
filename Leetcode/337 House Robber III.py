459de5b7c174846f17d12737855144076ddb0555
ShengYg <shengy0726@gmail.com>
2016-12-29 21:27:43 +0800

    dp

--- Leetcode/337 House Robber III.py
+++ Leetcode/337 House Robber III.py
@@ -19,3 +19,25 @@ class Solution(object):
         if root.right:
             val += self.rob(root.right.left) + self.rob(root.right.right)
         return max(val + root.val, self.rob(root.left) + self.rob(root.right))
+
+
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return self.robsub(root, {})
+    def robsub(self, root, nodemap):
+        if not root:
+            return 0
+        if root in nodemap:
+            return nodemap[root]
+        val = 0
+        if root.left:
+            val += self.robsub(root.left.left, nodemap) + self.robsub(root.left.right, nodemap)
+        if root.right:
+            val += self.robsub(root.right.left, nodemap) + self.robsub(root.right.right, nodemap)
+        val = max(val + root.val, self.robsub(root.left, nodemap) + self.robsub(root.right, nodemap))
+        nodemap[root] = val
+        return val
