9f7e9b870d8e04d2f21cae230248f2c28689d623
ShengYg <shengy0726@gmail.com>
2016-12-29 21:27:16 +0800

    dp

---
+++ Leetcode/337 House Robber III.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        val = 0
+        if root.left:
+            val += self.rob(root.left.left) + self.rob(root.left.right)
+        if root.right:
+            val += self.rob(root.right.left) + self.rob(root.right.right)
+        return max(val + root.val, self.rob(root.left) + self.rob(root.right))
