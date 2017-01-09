c4a0bb0222e4d002249b9630350b20b6c87662fa
ShengYg <shengy0726@gmail.com>
2017-01-09 19:22:11 +0800

    hash

---
+++ Leetcode/349 Intersection of Two Arrays.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        ret = []
+        for item in nums1:
+            if item in nums2 and item not in ret:
+                ret.append(item)
+        return ret
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1).intersection(set(nums2)))
