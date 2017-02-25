6c3f7f96a216986f6d154e1a2b1fe10f3ab87852
ShengYg <shengy0726@gmail.com>
2017-02-25 11:00:56 +0800

    Binary Search

---
+++ Leetcode/374 Guess Number Higher or Lower.java
@@ -0,0 +1,20 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        return bsearch(1, n);
+    }
+    private int bsearch(int start, int end){
+        if(start > end) 
+            return -1;//R u kidding me?
+        if(guess(start) == 0) 
+            return start;
+        if(guess(end) == 0) 
+            return end;
+        int mid = start + (end - start) / 2;
+        if(guess(mid) == 0) 
+            return mid;
+        else if(guess(mid) == -1) 
+            return bsearch(start + 1, mid - 1);
+        else 
+            return bsearch(mid + 1, end - 1);
+    }
+}
