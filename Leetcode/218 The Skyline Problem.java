1138ba1a7917bfd6cc8380cc9cff1c88e52298ca
ShengYg <shengy0726@gmail.com>
2017-03-04 11:41:23 +0800

    heap

---
+++ Leetcode/218 The Skyline Problem.java
@@ -0,0 +1,72 @@
+public class Solution {
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> result = new ArrayList<>();
+        List<int[]> height = new ArrayList<>();
+        for(int[] b:buildings) {
+            height.add(new int[]{b[0], -b[2]});
+            height.add(new int[]{b[1], b[2]});
+        }
+        Collections.sort(height, (a, b) -> {
+            if(a[0] != b[0])
+                return a[0] - b[0];
+            return a[1] - b[1];
+        });
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+        pq.offer(0);
+        int prev = 0;
+        for(int[] h:height) {
+            if(h[1] < 0) {
+                pq.offer(-h[1]);
+            } else {
+                pq.remove(h[1]);
+            }
+            int cur = pq.peek();
+            if(prev != cur) {
+                result.add(new int[]{h[0], cur});
+                prev = cur;
+            }
+        }
+        return result;
+    }
+}
+
+public class Solution {
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> heights = new ArrayList<>();
+        for(int[] b:buildings) {
+            heights.add(new int[]{b[0], -b[2]});
+            heights.add(new int[]{b[1], b[2]});
+        }
+        Collections.sort(heights, (a, b) -> {
+            if(a[0] != b[0])
+                return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        TreeMap<Integer, Integer> heightMap = new TreeMap<>(Collections.reverseOrder());
+        heightMap.put(0,1);
+        int prevHeight = 0;
+        List<int[]> skyLine = new LinkedList<>();
+
+        for (int[] h: heights) {
+            if (h[1] < 0) {
+                Integer cnt = heightMap.get(-h[1]);
+                cnt = ( cnt == null ) ? 1 : cnt + 1;
+                heightMap.put(-h[1], cnt);
+            } else {
+                Integer cnt = heightMap.get(h[1]);
+                if (cnt == 1) {
+                    heightMap.remove(h[1]);
+                } else {
+                    heightMap.put(h[1], cnt - 1);
+                }
+            }
+            int currHeight = heightMap.firstKey();
+            if (prevHeight != currHeight) {
+                skyLine.add(new int[]{h[0], currHeight});
+                prevHeight = currHeight;
+            }
+        }
+        return skyLine;
+    }
+}
