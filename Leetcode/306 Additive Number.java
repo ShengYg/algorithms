0643e76e4a185f87c45d3d473f3e69169c29d277
ShengYg <shengy0726@gmail.com>
2017-03-10 14:06:30 +0800

    dp

---
+++ Leetcode/306 Additive Number.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean isAdditiveNumber(String num) {
+        int n = num.length();
+        for(int i = 1; i <= n/2; i++)
+            for(int j = 1; i+j+Math.max(i, j) <= n; j++)
+                if(isValid(i, j, num))
+                    return true;
+        return false;
+    }
+    public boolean isValid(int i, int j, String num){
+        if (num.charAt(0) == '0' && i > 1) return false;
+        if (num.charAt(i) == '0' && j > 1) return false;
+        String sum;
+        Long x1 = Long.parseLong(num.substring(0, i));
+        Long x2 = Long.parseLong(num.substring(i, i+j));
+        for(int start = i+j; start != num.length(); start += sum.length()){
+            x2 = x1 + x2;
+            x1 = x2 - x1;
+            sum = x2.toString();
+            if(!num.startsWith(sum, start))
+                return false;
+        }
+        return true;
+    }
+}
