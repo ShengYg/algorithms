ca2b4d522657d61a2e0d5a03203154a52c534f56
ShengYg <shengy0726@gmail.com>
2017-03-16 16:29:27 +0800

    greedy

---
+++ Leetcode/330 Patching Array.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int i = 0, added = 0;
+        while (miss <= n) {
+            if (i < nums.length && miss >= nums[i])
+                miss += nums[i++];
+            else {
+                miss += miss;
+                added++;
+            }
+        }
+        return added;
+    }
+}
