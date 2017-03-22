698ce5f7aa6af455275ef7899e532a65960483c1
ShengYg <shengy0726@gmail.com>
2017-03-22 16:27:11 +0800

    dp

---
+++ Leetcode/403 Frog Jump.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean canCross(int[] stones) {
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>(stones.length);
+        map.put(0, new HashSet<Integer>());
+        map.get(0).add(1);
+        for(int i = 1; i < stones.length; i++)
+            map.put(stones[i], new HashSet<Integer>());
+        for(int i = 0; i < stones.length - 1; i++){
+            int stone = stones[i];
+            for(int step: map.get(stone)){
+                int reach = step + stone;
+                if(reach == stones[stones.length - 1])
+                    return true;
+                HashSet<Integer> set = map.get(reach);
+                if(set != null){
+                    set.add(step);
+                    if (step - 1 > 0) set.add(step - 1);
+                    set.add(step + 1);
+                }
+            }
+        }
+        return false;
+    }
+}
