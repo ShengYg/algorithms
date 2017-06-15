388825c07ba2d8b586cd9a73382a8673c31c029a
ShengYg <shengy0726@gmail.com>
2017-06-15 11:06:02 +0800

    array

--- Leetcode/077 Combinations.py
+++ Leetcode/077 Combinations.py
@@ -15,3 +15,14 @@ class Solution(object):
             return
         for i in range(start, n + 1):
             self.comb(out, list + [i], i + 1, n, k - 1)
+
+
+from itertools import combinations
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return list(combinations(range(1, n+1), k))
