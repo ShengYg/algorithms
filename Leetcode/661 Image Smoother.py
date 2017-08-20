4d4773fd6b501cad3a2a31d4c8428c931fe6eb17
ShengYg <shengy0726@gmail.com>
2017-08-20 11:08:34 +0800

    array

---
+++ Leetcode/661 Image Smoother.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+class Solution(object):
+    def imageSmoother(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        n, m = len(M), len(M[0])
+        res = [[0] * m for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                s, cnt = 0, 0
+                for move_i in [-1,0,1]:
+                    for move_j in [-1,0,1]:
+                        resi = i + move_i
+                        resj = j + move_j
+                        if resi >= 0 and resj >= 0 and resi < n and resj < m:
+                            s += M[resi][resj]
+                            cnt += 1
+                res[i][j] = s / cnt
+        return res
